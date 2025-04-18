391f1af26abd7bc2039fc6cdbb5149a000bcf357
Taufik Pragusga <taufik@pragusga.com>
2025-04-18 19:03:17 +0700

    chore(djatoza): use dotenv to load configuration

--- djatoza/settings.py
+++ djatoza/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default="django-insecure-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", default="True").lower() == "true"
 
 ALLOWED_HOSTS = []
 
